872adf89282eae33b72e2577b37715130a12571f
XShine555 <xshine555@gmail.com>
2026-04-24 22:29:08 +0200

    Adjust things

--- app/components/game-card-stats.tsx
+++ app/components/game-card-stats.tsx
@@ -8,7 +8,7 @@ type GameCardStatsProps = {
 
 export default function GameCardStats({ title, description, icon }: GameCardStatsProps) {
     return (
-        <div className="flex h-full w-full items-center overflow-hidden rounded-lg bg-bg-200 shadow-md border border-bg-400">
+        <div className="flex h-full w-full items-center overflow-hidden rounded-xl bg-bg-200 shadow-md border border-bg-500">
             <div className="flex flex-1 flex-col text-left p-6">
                 <h2 className="text-sm uppercase text-text-400">{title}</h2>
                 <p className="mt-2 text-3xl">{description}</p>
