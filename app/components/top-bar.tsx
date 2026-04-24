ae2af504628d8437187e92955ea9cca74d3d14bc
XShine555 <xshine555@gmail.com>
2026-04-24 22:30:10 +0200

    Adjust color

--- app/components/top-bar.tsx
+++ app/components/top-bar.tsx
@@ -1,6 +1,6 @@
 export default function TopBar() {
     return (
-        <div className="w-full h-20 bg-bg-100 border-b border-bg-300 flex items-center justify-between px-4">
+        <div className="w-full h-20 bg-bg-100 border-b border-bg-500 flex items-center justify-between px-4">
         </div>
     )
 }
