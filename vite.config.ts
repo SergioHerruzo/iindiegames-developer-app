4306a30c17bd3848e0ebcabd1a4bf004a0b02077
XShine555 <xshine555@gmail.com>
2026-04-25 17:38:31 +0200

    Fix error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+      define: {
+        global: {},
+      },
+    }
+    : {}),
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
