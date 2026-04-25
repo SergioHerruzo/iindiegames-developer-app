3fe8addd50d87195069f0b70e2da520b24f0ea4f
XShine555 <xshine555@gmail.com>
2026-04-25 17:44:00 +0200

    Remove vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  ...(process.env.NODE_ENV === 'development'
-    ? {
-      define: {
-        global: {},
-      },
-    }
-    : {}),
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
