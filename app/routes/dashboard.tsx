56c9dbe763ae9630a74d4e646b957144989bf4a7
XShine555 <xshine555@gmail.com>
2026-04-25 15:58:47 +0200

    Adjustments

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -19,7 +19,7 @@ export default function Dashboard() {
                         <h1 className="text-4xl">Mis juegos</h1>
                         <h3 className="text-sm text-text-400">Gestiona tus juegos, compilaciones y análisis.</h3>
                     </div>
-                    <button className="text-sm self-center inline-flex items-center justify-center px-4 py-2 bg-primary-400 text-text-100 rounded-full hover:bg-primary-500 transition-colors cursor-pointer">
+                    <button className="text-sm self-center inline-flex items-center justify-center px-4 py-2 bg-primary-400 text-text-100 rounded-full hover:bg-primary-500 transition-colors cursor-pointer transition-transform hover:-translate-y-1">
                         <Plus className="w-6 h-6 mr-2" />
                         Agregar juego
                     </button>
