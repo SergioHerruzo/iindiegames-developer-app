aa9bb552136e76228b48d10924ae0a457c1277a0
XShine555 <xshine555@gmail.com>
2026-04-25 16:36:27 +0200

    Fix border

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -5,11 +5,11 @@ export default function TopBar() {
                 <a href="/dashboard" className="text-2xl font-space-grotesk text-text-200">Indie Games</a>
             </div>
             <div className="flex items-center justify-start gap-4">
-                <button className="border border-bg-300 cursor-pointer">
+                <button className="cursor-pointer">
                     <img
                         src="https://preview.redd.it/ce-spuneti-de-dictatorul-mbappe-v0-k8l241njsqtg1.jpeg?width=451&format=pjpg&auto=webp&s=860149b0068c2f1b1efd438b35b9a8154b6a3f93"
                         alt="User Avatar"
-                        className="h-10 w-10 rounded-full object-cover"
+                        className="h-10 w-10 rounded-full object-cover border border-bg-300"
                     />
                 </button>
             </div>
